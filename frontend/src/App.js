022edc96522d88f6dc73f5ab2d82d0de146a3028
Samuel <sumlebulle@hotmail.com>
2021-01-14 20:10:33 +0100

    Added html & styling for login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
-  return <>Welcome to MERN Chat</>
+  return (
+    <Router>
+      <main>
+        <Route path='/' component={LoginScreen} />
+      </main>
+    </Router>
+  )
 }
 
 export default App
